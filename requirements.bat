2f99b44a76e79cc9731cb73b829123d315dbfcfb
That Developer Guy <146465807+That-Developer-Guy@users.noreply.github.com>
2024-07-20 12:41:31 +0200

    Update requirements.bat

--- requirements.bat
+++ requirements.bat
@@ -1,7 +1,4 @@
 @echo off
-if exist "C:\Users\%USERNAME%\.xpi-extract" (
-    del /F /Q C:\Users\%USERNAME%\.xpi-extract
-)
 if not exist "C:\Users\%USERNAME%\.xpi-extract" (
     mkdir "C:\Users\%USERNAME%\.xpi-extract"
 )
